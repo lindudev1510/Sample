2ad9c261bbfe46bdbc01a91ca4518827df7aea28
lindudev1510 <lindusijin123@gmail.com>
2020-06-18 12:51:36 +0530

    changes

--- src/test/java/org/test/LaunchBrowser.java
+++ src/test/java/org/test/LaunchBrowser.java
@@ -1,5 +1,11 @@
 package org.test;
 
 public class LaunchBrowser {
+	public static void main(String[] args) {
+		System.out.println("launch");
+		System.out.println("Browser");
+		System.out.println("branches");
+		
+	}
 
 }
